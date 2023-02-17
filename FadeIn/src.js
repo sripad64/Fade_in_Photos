25bc6a6322da4321537024b8d387532f2c6ff856
sripad64 <102305777+sripad64@users.noreply.github.com>
2023-02-17 15:53:02 +0530

    Add files via upload

---
+++ FadeIn/src.js
@@ -0,0 +1,13 @@
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            document.querySelectorAll(".animated")[0].classList.add("fadeInLeft");
+            document.querySelectorAll(".animated")[1].classList.add("fadeInTop");
+            document.querySelectorAll(".animated")[2].classList.add("fadeInBottom");
+            document.querySelectorAll(".animated")[3].classList.add("fadeInRight");
+        }
+    })
+
+})
+
+observer.observe(document.querySelector(".container"));
